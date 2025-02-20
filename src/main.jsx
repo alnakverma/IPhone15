58024738ae5f774c21b02c2db76a750a80ab67ed
alnakverma <anujkumarverma1010@gmail.com>
2025-02-20 18:32:43 +0530

    completed

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://4faf2b52eae5faafc33a3e97d597bcb3@o4508845992968192.ingest.de.sentry.io/4508845996900432",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+
